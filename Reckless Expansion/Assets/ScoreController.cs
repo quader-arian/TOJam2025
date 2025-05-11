b0aa044dcd81fab1ea827fc4c32aeb4e2c04a100
quader-arian <arianq@my.yorku.ca>
2025-05-10 20:20:19 -0400

    Timer and score being fixed rn

--- Reckless Expansion/Assets/ScoreController.cs
+++ Reckless Expansion/Assets/ScoreController.cs
@@ -1,28 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using TMPro;
 using UnityEngine;
 
 public class ScoreController : MonoBehaviour
 {
 
-    public float seconds;
-    public float minutes;
-    public int score;
+    public float score;
+    public float money;
     public float health;
     public int rooms;
     public float timer = 0;
-    private TMP_Text m_TextComponent;
+    public TMP_Text tmpt;
+    public int lastSeconds = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime * 600;
+
+        int seconds = (int)(timer % 60);
+        int minutes = (int)(timer / 60);
+        int hours = (int)(timer / 3600);
+
+        string timerString = string.Format("{0:0}:{1:00}:{2:00}", hours, minutes, seconds);
+        rooms = GameObject.FindGameObjectsWithTag("Place").Length;
+        if(seconds != lastSeconds)
+        {
+            score += rooms * 55;
+            money += rooms * 35;
+            lastSeconds = seconds;
+        }
+
+        tmpt.text = "Time: " + timerString + "\nScore: " + score + "\nStellar Marks: $" + money + "\nRooms: " + rooms;
     }
 }
